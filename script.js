45deda5a72dd6582c014f8d4f07c211160b06e86
nobayokai <estuz.zutse@gmail.com>
2026-06-14 14:04:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -231,7 +231,7 @@ document.addEventListener("DOMContentLoaded", () => {
             if (page === "nilai") {
                 contentArea.innerHTML = `
                     <iframe 
-                        src="Nilai/index.html" 
+                        src="https://erapor.novaharyanto.my.id/" 
                         style="width: 100%; height: 85vh; border: none; border-radius: 8px; box-shadow: 0 4px 10px rgba(0,0,0,0.1);" 
                         title="Aplikasi e-Rapor">
                     </iframe>
